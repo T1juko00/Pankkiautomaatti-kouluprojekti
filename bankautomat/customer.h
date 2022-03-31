2dc7a71951ab8151f709486ca7c5667c36a402b9
t1juko00 <t1juko00@students.oamk.fi>
2022-03-31 10:48:44 +0300

    login form ja customer tehty, ei yhteyttä restapiin

---
+++ bankautomat/customer.h
@@ -0,0 +1,25 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#include <QDialog>
+
+namespace Ui {
+class Customer;
+}
+
+class Customer : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Customer(QString CustUsername,QString Customerid,QWidget *parent = nullptr);
+    ~Customer();
+
+private slots:
+    void on_btnShowtransactions_clicked();
+
+private:
+    Ui::Customer *ui;
+};
+
+#endif // CUSTOMER_H
