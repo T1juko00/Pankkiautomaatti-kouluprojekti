c2db7d54f16d090cd47c78a61c0430fb87bdac32
t1juko00 <t1juko00@students.oamk.fi>
2022-04-12 11:57:53 +0300

    päivittelyä qt

--- api/database.js
+++ api/database.js
@@ -2,11 +2,7 @@ const mysql = require('mysql');
 const connection = mysql.createPool({
   host: 'localhost',
   user: 'root',
-<<<<<<< HEAD
   password: 'veleho_123',
-=======
-  password: 'Kalamiehen2toveri?',
->>>>>>> 903923fea8ad51008ca09c788cd956e8e7c3595a
   database: 'bankautomat'
 });
 module.exports = connection;
